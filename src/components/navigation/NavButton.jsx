b4dd7f356a4af02066629a7a75824b3b003f78c4
戴森球 <3289990188@qq.com>
2025-03-19 23:06:20 +0800

    NavButton旋转修复

--- src/components/navigation/NavButton.jsx
+++ src/components/navigation/NavButton.jsx
@@ -35,26 +35,28 @@ const NvaButton=({x,y,label,link,icon,newTab})=>{
     <div className="absolute cursor-pointer z-50"
     style={{transform:`translate(${x},${y})`}}
     >
-       <NavLink 
-       variants={item}
-       herf={link} 
-       target={newTab? "_blank":"_self"}
-       className="text-foreground rounded-full flex items-center justify-center
-       custom-bg
-       "
-       aria-label={label}
-       name={label}
-       prefetch={false}
-       scroll={false}
-       >
-        <span className="relative w-14 h-14 animate-spin-slow-reverse group-hover:pause hover:text-accent">
-            {getIcon(icon)}
-            <span className="peer bg-transparent absolute top-0 left-0 w-full h-full"/>
-            <span className="absolute hidden peer-hover:block px-2 py-1 left-full mx-2 top-1/2 -translate-y-1/2 bg-background text-foreground text-sm rounded-md shadow-lg whitespace-nowrap">
-            {label}
-            </span> 
-        </span>
-       </NavLink>
+        <NavLink
+              variants={item}
+              href={link}
+              target={newTab ? "_blank" : "_self"}
+              className="text-foreground  rounded-full flex items-center justify-center
+        custom-bg
+        "
+              aria-label={label}
+              name={label}
+              prefetch={false}
+              scroll={false}
+            >
+              <span className="relative  w-14 h-14 p-4 animate-spin-slow-reverse group-hover:pause hover:text-accent">
+                {getIcon(icon)}
+
+                <span className="peer bg-transparent absolute top-0 left-0 w-full h-full" />
+
+                <span className="absolute hidden peer-hover:block px-2 py-1 left-full mx-2 top-1/2 -translate-y-1/2 bg-background text-foreground text-sm rounded-md shadow-lg whitespace-nowrap">
+                  {label}
+                </span>
+              </span>
+            </NavLink>
      </div>
     )
 }
